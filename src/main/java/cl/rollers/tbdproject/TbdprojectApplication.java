34e0383e7c2e8243cee63c61ec16f9419d41ac4c
Jose <jose.maureira.b@usach.cl>
2019-08-26 17:42:48 -0400

    Init: First commit

---
+++ src/main/java/cl/rollers/tbdproject/TbdprojectApplication.java
@@ -0,0 +1,13 @@
+package cl.rollers.tbdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbdprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbdprojectApplication.class, args);
+    }
+
+}

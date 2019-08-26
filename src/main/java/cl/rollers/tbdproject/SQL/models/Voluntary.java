66915931d1f20e0a8faba3590ef2cc3e0eec3817
Christian <christian.chamblat@usach.cl>
2019-08-26 18:40:34 -0400

    Init: Class Voluntary
    Se inicializa la clase Voluntario.

---
+++ src/main/java/cl/rollers/tbdproject/SQL/models/Voluntary.java
@@ -0,0 +1,48 @@
+package cl.rollers.tbdproject.SQL.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "voluntary")
+
+public class Voluntary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "VOLUNTARY_SEQ")
+    @SequenceGenerator(name = "VOLUNTARY_SEQ", sequenceName = "SEQ_VOLUNTARY", allocationSize = 1)
+    private Integer id;
+
+    @NonNull
+    @Column(name = "name")
+    private String name;
+
+    @NonNull
+    @Column(name = "last_Name")
+    private String lastName;
+
+    @NonNull
+    @Column(name = "rut")
+    private String rut;
+
+    @NonNull
+    @Column(name = "age")
+    private Integer age;
+
+
+
+
+
+
+
+
+
+
+
+}

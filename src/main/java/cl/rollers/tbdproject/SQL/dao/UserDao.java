78a042b4b4e66d26569b5b802b663baff8530aea
Jose <jose.maureira.b@usach.cl>
2019-09-21 22:41:35 -0300

    Fix: findUserById

--- src/main/java/cl/rollers/tbdproject/SQL/dao/UserDao.java
+++ src/main/java/cl/rollers/tbdproject/SQL/dao/UserDao.java
@@ -5,4 +5,5 @@ import cl.rollers.tbdproject.SQL.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User, Integer> {
+    User findUserById(Integer id);
 }
